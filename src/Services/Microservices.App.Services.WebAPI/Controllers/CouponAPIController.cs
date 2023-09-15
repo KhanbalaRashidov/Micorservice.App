cecc841f2c32c739950501adcf4048f6bb3ef0bc
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2023-09-15 17:35:15 +0400

    Add CRUD

--- src/Services/Microservices.App.Services.WebAPI/Controllers/CouponAPIController.cs
+++ src/Services/Microservices.App.Services.WebAPI/Controllers/CouponAPIController.cs
@@ -1,9 +1,8 @@
 ﻿using AutoMapper;
 using Microservices.App.Services.WebAPI.Data;
+using Microservices.App.Services.WebAPI.Models;
 using Microservices.App.Services.WebAPI.Models.Dtos;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Microservices.App.Services.WebAPI.Controllers
 {
@@ -29,7 +28,7 @@ namespace Microservices.App.Services.WebAPI.Controllers
             {
                 var coupons = this.appDbContext.Coupons.ToList();
 
-                responseDto.Result = this.mapper.Map<CouponDto>(coupons);
+                responseDto.Result = this.mapper.Map<IEnumerable<CouponDto>>(coupons);
             }
             catch (Exception ex)
             {
@@ -50,6 +49,83 @@ namespace Microservices.App.Services.WebAPI.Controllers
                 var coupon = this.appDbContext.Coupons.First(x => x.CouponId == id);
 
                 responseDto.Result = this.mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                responseDto.IsSuccess = false;
+                responseDto.Message = ex.Message;
+            }
+
+            return responseDto;
+        }
+        
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                var coupon = this.appDbContext.Coupons.First(x => x.CouponCode.ToLower()==code.ToLower());
+                
+                responseDto.Result = this.mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                responseDto.IsSuccess = false;
+                responseDto.Message = ex.Message;
+            }
+
+            return responseDto;
+        }
+        
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var coupon = this.mapper.Map<Coupon>(couponDto);
+                this.appDbContext.Coupons.Add(coupon);
+                this.appDbContext.SaveChanges();
+                
+                responseDto.Result = this.mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                responseDto.IsSuccess = false;
+                responseDto.Message = ex.Message;
+            }
+
+            return responseDto;
+        }
+        
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var coupon = this.mapper.Map<Coupon>(couponDto);
+                this.appDbContext.Coupons.Update(coupon);
+                this.appDbContext.SaveChanges();
+                
+                responseDto.Result = this.mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                responseDto.IsSuccess = false;
+                responseDto.Message = ex.Message;
+            }
+
+            return responseDto;
+        }
+        
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                var coupon = this.appDbContext.Coupons.First(coupon=>coupon.CouponId==id);
+                this.appDbContext.Coupons.Remove(coupon);
+                this.appDbContext.SaveChanges();
             }
             catch (Exception ex)
             {
